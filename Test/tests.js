9eb44fe8d472f12b73ddadd8c53367c230bbcbea
samSterns <sam.g.sterns@gmail.com>
2019-09-19 20:56:06 -0700

    commit end day 4

---
+++ Test/tests.js
@@ -0,0 +1,14 @@
+import isEven from '../is-even';
+const test = QUnit.test;
+
+test('time to test!', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    // What should the result be?
+    assert.equal(true, false);
+});
---
+++ is-even.js
@@ -0,0 +1,14 @@
+
+
+
+
+function isEven(number) {
+    const remainder = number % 2;
+  
+    if(remainder === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
